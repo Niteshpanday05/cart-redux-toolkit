19269bd25b8ebbd40a59a0a93f6722d988bc64ec
Niteshpanday05 <eagleseye.np@gmail.com>
2026-02-04 18:37:41 +0545

    implementing logic

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
+  useSelector(state=>console.log(state.cart))
   return (
     <div className="wrapper">
       <div>
